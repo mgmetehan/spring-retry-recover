50178ec17c3318173cb8b2aca70ff329712e7284
mgmetehan <mgmetehan@hotmail.com>
2024-06-02 15:32:55 +0300

    init projects

---
+++ src/main/java/com/mgmetehan/springretryrecover/SpringRetryRecoverApplication.java
@@ -0,0 +1,13 @@
+package com.mgmetehan.springretryrecover;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRetryRecoverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRetryRecoverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mgmetehan/springretryrecover/SpringRetryRecoverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mgmetehan.springretryrecover;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRetryRecoverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
